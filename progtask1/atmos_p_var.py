659957435ab5f5b8e8195143aada1070b96f6ec5
neaschr <neaschr@yahoo.com>
2025-02-20 18:03:33 +0100

    Added ideal gas law

--- progtask1/atmos_p_var.py
+++ progtask1/atmos_p_var.py
@@ -28,8 +28,8 @@ atomic_mass_unit = spc.physical_constants['atomic mass constant'][0] #kg
 data = data_reader('MSIS.dat')
 
 #Finding the number density as a sum of each species number density, given as number per cm cubed
-number_density = data['Oksygen-atoms_cm-3'].to_numpy() + data['Nitrogen-molecules_cm-3'].to_numpy() 
-+ data['Oksygen-molecules_cm-3'].to_numpy()
+number_density = (data['Oksygen-atoms_cm-3'].to_numpy() + data['Nitrogen-molecules_cm-3'].to_numpy() 
++ data['Oksygen-molecules_cm-3'].to_numpy())
 
 #Calculating the average particle mass using the mass density and number density, mass in grams
 avg_particle_mass = data['Mass_density_g_per_cm3'].to_numpy() / number_density
@@ -52,9 +52,21 @@ pressure_at_0 = 101300 #Pa
 #Finding the pressure array for the atmosphere
 atmospheric_pressure = barometric_equation(pressure_at_0, avg_particle_mass, heights_in_km, grav_acc_var, temperature_K)
 
+#We will also calculate the pressure according to the ideal gas law using the temperatures and density profiles in the data file
+def ideal_gas_law(number_density_per_cm3: np.ndarray, temperatures: np.ndarray) -> np.ndarray:
+
+    k_B = spc.k
+    number_density_per_meter3 = number_density_per_cm3 * 1e6
+    pressure = (number_density_per_meter3 * k_B * temperatures) 
+
+    return pressure
+
+atmospheric_pressure_ideal = ideal_gas_law(number_density, temperature_K)
+
 #Plotting...
 fig, ax = plt.subplots(figsize = (9, 7))
-ax.plot(atmospheric_pressure, heights_in_km, linestyle = 'solid')
+ax.semilogx(atmospheric_pressure, heights_in_km, linestyle = 'solid', label = 'Barometric equation')
+ax.semilogx(atmospheric_pressure_ideal, heights_in_km, linestyle = 'dashed', label = 'Ideal gas law')
 
 #Plot aesthetics
 ax.set_ylabel("Height (km)")
@@ -62,4 +74,5 @@ ax.set_xlabel("Pressure (Pa)")
 ax.set_title("Pressure as a function of height")
 ax.grid(True)
 
+plt.legend()
 plt.show()
