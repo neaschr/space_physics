6e3e2abef50305ee684201dd77f20d28200534d5
neaschr <neaschr@yahoo.com>
2025-02-24 14:20:10 +0100

    Started a new task

---
+++ progtask2/optical_depth.py
@@ -0,0 +1,9 @@
+import numpy as np
+from scipy import constants as spc
+import matplotlib.pyplot as plt
+
+#We will make functions to find the optical depth as a function of altitude and wavelength
+
+def optical_depth():
+
+    pass
